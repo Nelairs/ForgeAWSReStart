639c8ede35713271ac8f6c41d4888fb2fce0c6b3
Nelairs <saetchenique@gmail.com>
2022-09-29 19:50:05 -0300

    DESAFIO - Creacion de variables

---
+++ Etchenique_Santiago_JS_CrearVariables.js
@@ -0,0 +1,12 @@
+/*
+MVP
+
+-Identificar variables para determinar si persona sube o no 
+
+
+*/
+
+let height = 0; //  SE DECALRA VARIABLE DE ALTURA QUE SERA NUMERICA
+
+let age =   0;  // SE DECLARA VARIABLE DE LA EDAD QUE SERA NUMERICA
+
