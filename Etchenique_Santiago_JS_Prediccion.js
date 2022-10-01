34ada2f267a417b3721fd35eab266480b2765bb7
Nelairs <saetchenique@gmail.com>
2022-10-01 19:37:02 -0300

    Desafio - Prediccion

---
+++ Etchenique_Santiago_JS_Prediccion.js
@@ -0,0 +1,15 @@
+// Predicción 1: Qué indicará console.log cuando se llame a esta función?
+// > Naci en 1980
+
+
+// Predicción 2: Si necesitáramos enviar un año de nacimiento a la función, le diríamos a la función "oye, te enviaremos una variable llamada birthYearInput". Lo hacemos agregando el nombre de la variable entre paréntesis. Mira a continuación un ejemplo. 
+// Entonces, si la variable birthYearInput es 1980 (también conocida como var birthYearInput = 1980), y se llama a esta función, ¿qué indicará console.log?
+// Si le pasamos 1980 dira > Naci en 1980
+
+
+// Predicción 3: Intentémoslo otra vez. Si var num1 = 10 y var num2 = 20, ¿qué indicaría console.log?
+// La consola imprimira 
+// >Sumando numeros!
+// >num1 es: 10
+// >num2 es: 20
+// >30
